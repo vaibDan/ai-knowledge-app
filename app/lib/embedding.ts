48393caf3021438a5bbab0cf3d8e81d96651ad22
Vaibhav Dange <vaibhavdange23@gmail.com>
2026-05-08 13:35:26 +0530

    fix:updated the embedding model

--- app/lib/embedding.ts
+++ app/lib/embedding.ts
@@ -1,29 +1,31 @@
-// import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
 
-// export async function generateEmbedding(text: string) {
-//     const model = genAI.getGenerativeModel({
-//         model: "gemini-embedding-004",
-//     });
-
-//     const result = await model.embedContent(text);
-
-//     return result.embedding.values;
-// }
-
-export async function generateEmbedding(text: string): Promise<number[]> {
-    const res = await fetch("http://localhost:11434/api/embeddings", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            model: "nomic-embed-text",
-            prompt: text,
-        }),
+export async function generateEmbedding(text: string) {
+    const model = genAI.getGenerativeModel({
+        model: "gemini-embedding-001",
     });
 
-    const data = await res.json();
-    return data.embedding;
+    const result = await model.embedContent(text);
+
+    return result.embedding.values;
 }
+
+// const OLLAMA_URL = process.env.OLLAMA_URL || "http://localhost:11434";
+
+// export async function generateEmbedding(text: string): Promise<number[]> {
+//     const res = await fetch(`${OLLAMA_URL}/api/embeddings`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             model: "nomic-embed-text",
+//             prompt: text,
+//         }),
+//     });
+
+//     const data = await res.json();
+//     return data.embedding;
+// }
